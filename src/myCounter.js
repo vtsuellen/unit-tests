7115f3ab9745e01c8007e2f25b391c290f675988
vtsuellen <vtsuellen@hotmail.com>
2023-05-08 19:29:43 -0300

    arrumei

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,10 +16,10 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var i = 2; i <= 3; i += 1) {
+    for (let i = 2; i <= 3; i += 1) {
       myArray.push(i);
     }
   }
