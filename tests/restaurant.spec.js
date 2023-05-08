17b66b425602d8e81ed01f5d33d7055757a13e25
vtsuellen <vtsuellen@hotmail.com>
2023-05-08 19:23:41 -0300

    .

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -2,7 +2,6 @@ const createMenu = require('../src/restaurant');
  
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // Escreva todos os testes aqui.
+  
   });
 });
